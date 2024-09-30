10a9a2f861fe7f2a74ccbb74f3b75e057e2e1993
Sean <363179492@qq.com>
2024-09-30 12:44:49 +0800

    bug

--- src/helpers/buttonEvent.ts
+++ src/helpers/buttonEvent.ts
@@ -1,11 +1,39 @@
 import { ModelStates } from "../scene";
-
+const host = "http://localhost:3000";
+// 分割
 export function splitPly(modelStates: ModelStates) {
     if (!modelStates.model) return;
     console.log("Splitting PLY");
     postMeshDataToServer(modelStates);
 }
 
+//加载文件列表
+export function importModel(e: HTMLInputElement) {
+    console.log("Importing Model");
+    if(e == null || e.files == null || e.files.length == 0) return;
+    let directoryHandle = e.files;
+    var list:string[] = [];
+    var files = ""
+    for (let i = 0; i < directoryHandle.length; i++) {
+        let file = directoryHandle[i];
+        list.push(file.name);
+        // console.log(file);
+        files += file.name + "<br/>";
+    }
+    $("#fileList").html(files);
+}
+
+//处理文件列表
+export function genFiles(e: HTMLInputElement) {
+    console.log("genFiles Model");
+    if(e == null || e.files == null || e.files.length == 0) return;
+    var list:string[] = [];
+    for (let i = 0; i < e.files.length; i++) {
+        let file = e.files[i];
+        list.push(file.name);
+    }
+    postFilesToServer(list);
+}
 function postMeshDataToServer(modelStates: ModelStates) {
     if (!modelStates.model) return;
     const geometry = modelStates.model.geometry;
@@ -13,12 +41,30 @@ function postMeshDataToServer(modelStates: ModelStates) {
     const colors = geometry.getAttribute('color').array.buffer;
     const normals = geometry.getAttribute('normal').array.buffer;
     $.ajax({
-        url: 'http://localhost:3000/split',
+        url: `${host}/split`,
         type: 'POST',
         data: {
             vertices: vertices,
             colors: colors,
             normals: normals
+        },
+        success: function (response) {
+            console.log(response);
+        },
+        error: function (xhr, status, error) {
+            console.error(error);
+            console.error(xhr.responseText);
+            console.error(status);
+        }
+    });
+}
+
+function postFilesToServer(list: string[]) {
+    $.ajax({
+        url: `${host}/import`,
+        type: 'POST',
+        data: {
+            files: list
         },
         success: function (response) {
             console.log(response);
--- src/menu.ts
+++ src/menu.ts
@@ -2,7 +2,7 @@ import { Scene } from "three";
 import { PLYLoader } from "three/examples/jsm/loaders/PLYLoader";
 import { ModelStates } from "./scene";
 import { onFlyFileLoad } from "./helpers/plyLoader";
-import { splitPly } from "./helpers/buttonEvent";
+import { genFiles, importModel, splitPly } from "./helpers/buttonEvent";
 
 function bindButtonEvent(btn: string, callback: () => void) {
     $(btn).on("click", callback);
@@ -11,7 +11,11 @@ function bindButtonEvent(btn: string, callback: () => void) {
 export async function loadMenu(loader: PLYLoader, modelStates: ModelStates, scene: Scene) {
     // add new button event listener
     bindButtonEvent("#splitPly", splitPly.bind(null, modelStates));
-    
+    bindButtonEvent("#loadPlyDir", () => {
+        $("#fileInputDir").click();
+    });
+    bindButtonEvent("#genFiles", genFiles.bind(null, $("#fileInputDir").get(0) as HTMLInputElement));
+    $("#fileInputDir").on("change", importModel.bind(null, $("#fileInputDir").get(0) as HTMLInputElement));
     $("#fileInput").on("change", onFlyFileLoad.bind(null, loader, modelStates, scene));
 }
 
